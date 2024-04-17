db16fe2e36557b6b0c57e40ebb11e28dbfb2fcc2
Nina-Antoniuk <78640356+Nina-Antoniuk@users.noreply.github.com>
2024-04-17 17:33:14 +0300

    upd

---
+++ .next/static/SLS37ydiaXLJ1kS5Z7foO/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/d5ac097cb6269291.css","static/chunks/pages/index-cdd10d2151c6c855.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
