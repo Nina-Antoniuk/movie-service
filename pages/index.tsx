0797398c4b299460f7f54582ecea356c1f9618b7
Nina-Antoniuk <78640356+Nina-Antoniuk@users.noreply.github.com>
2024-04-18 07:55:17 +0300

    upd

--- pages/index.tsx
+++ pages/index.tsx
@@ -43,8 +43,13 @@ export default function Home() {
       <Head>
         <title>Movie Service</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          http-equiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <Layout progress={progress} resetQuizeStep={setQuizeStep}>
         {quizeStep !== LAST_QUIZE_STEP && (
           <form className="quizeForm">
